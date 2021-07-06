446698f5330329c8151924f7b8ecf45b8a154bc4
F1NH4WK <araujoplayer@gmail.com>
2021-07-06 13:49:41 -0300

    2.1.0

--- bd.py
+++ bd.py
@@ -2,7 +2,6 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-
 cred = credentials.Certificate('deschamps_bd.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
