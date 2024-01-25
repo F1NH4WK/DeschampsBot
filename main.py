773320c077a6c867137f5625f52016ffa5d7253a
Finhawk <eduardo.ar334@gmail.com>
2024-01-25 18:44:49 -0300

    feat: add quopri & utf-8 decode to news strings

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from imaplib import IMAP4_SSL
 from bs4 import BeautifulSoup
+import quopri
+from credentials import credentials
+
+email, password = credentials()
 
 def getEmailStream():
     with IMAP4_SSL('imap.gmail.com', 993) as M:
         M = IMAP4_SSL(host='imap.gmail.com', port=993)
-        M.login('botdeschamps@gmail.com', 'czra qfrn ztrz hzdc')
+        M.login(email, password)
         M.select(mailbox = 'INBOX')
         typ, data = M.search(None, 'ALL')
     
     for num in data[0].split():
         typ, stream = M.fetch(num, '(RFC822)')
-        return stream[0][1]
-    
-def getSoup(email_stream):
-    soup = BeautifulSoup(email_stream, 'html.parser')
-    return soup.prettify()
+    return stream[0][1]
 
-stream = getEmailStream()
-soup = getSoup(stream)
+email_stream = getEmailStream()
+
+def getDecodedArray(email_stream):
+    news_array = []
+    soup = BeautifulSoup(email_stream, 'lxml')
+    for bstring in soup.find_all('p')[1:]:
+        bstring_encoded = quopri.decodestring(bstring.getText())
+        news_array.append(bstring_encoded.decode('utf-8'))
+    return news_array
+
+news_array = getDecodedArray(email_stream)
