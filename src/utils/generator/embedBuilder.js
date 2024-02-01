c3e8d0dfe5939b4e1d84540f7bc088c7571964d4
Finhawk <eduardo.ar334@gmail.com>
2024-02-01 18:59:38 -0300

    feat: user error feedback & links to deschamps course

--- src/utils/generator/embedBuilder.js
+++ src/utils/generator/embedBuilder.js
@@ -13,15 +13,22 @@ export default async function getEmbed(){
     const embed = new EmbedBuilder()
 
     .setColor(Colors.Yellow)
+    .setAuthor({
+        name: 'Curso.dev',
+        url: 'https://curso.dev/'
+    })
     .setFooter({
-        text: 'Filipe Deschamps Newsletter',
-        iconURL: 'https://cdn.discordapp.com/avatars/852604893718118461/90c0563d288758efdd661e40b13a65b5.webp'
+        text: 'Delicinha!'
     })
     .setTimestamp(new Date().getTime())
     .setURL('https://filipedeschamps.com.br/newsletter')
     .setTitle('Inscreva-se na Newsletter!')
+    .setThumbnail('https://curso.dev/default-image-share.png')
 
     for (const notice of news){
+
+        let error_news = 0
+
         try{
             embed.addFields({
                 name: notice.title,
@@ -31,7 +38,11 @@ export default async function getEmbed(){
 
         catch(err){
             logger.error(`There's one or more news exceeding discord characters limit, ignoring them... 
-            Check out the file: ${news}`)
+            Check out the file: ${notice}`)
+            error_news++
+
+            embed
+                .setFooter({text: `${error_news} notícia${error_news > 1? 's' : ''} não ${error_news > 1? 'puderam': 'pode'} ser ${error_news > 1? 'enviadas': 'enviada'}...`})
         }
     }
     logger.info('Noticias armazenadas no embed!')
